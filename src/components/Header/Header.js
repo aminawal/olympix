6ced00b02d8dadf8e68140fe900654cb723ace4a
Abdullah Minawal <abdullah.minawal@hotmail.de>
2022-02-07 05:34:14 +0100

    update after cloud bugs

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import classes from './Header.module.css';
+
+const Header = (props) => {
+    return(
+        <header>
+            {props.children}
+        </header>
+    );
+};
+
+export default Header;
