6a56eacf71d7a4b36822d97fe53498a6f1af766c
Abdullah Minawal <abdullah.minawal@hotmail.de>
2022-02-07 07:25:15 +0100

    Create Header component and adding to Grid

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 
+import Logo from '../../assets/Logo/Logo.png';
+
 import classes from './Header.module.css';
 
 const Header = (props) => {
     return(
-        <header>
-            {props.children}
+        <header className={classes.header}>
+            <a href="/"><img src={Logo} alt="Olympix Logo" className={classes.logo}/></a>
+            <h1 className={classes.headline}>Headline</h1>
         </header>
     );
 };
